b6fa16a4717cf2a47322d432b8d5c877e176c422
David L. Inman <davidlevere@gmail.com>
2018-11-10 16:22:57 -0500

    make file minor ; feeling out console functions in c#

--- Program.cs
+++ Program.cs
@@ -1,11 +1,34 @@
 
+using System;
+
 namespace mu
 {
     public static class Program
     {
         public static void Main()
         {
+            MyClear(ConsoleColor.Black);
+            Console.CursorVisible = false;
+            DrawLetter( 'a', 5, 5, ConsoleColor.Blue, ConsoleColor.Black );
+            DrawLetter( 'b', 1, 1, ConsoleColor.Red, ConsoleColor.Black );
+            DrawLetter( 'c', 1, 5, ConsoleColor.Green, ConsoleColor.Black );
+            DrawLetter( 'd', 5, 1, ConsoleColor.Yellow, ConsoleColor.Black );
+            Console.Read();
+            Console.Clear();
+        }
 
+        private static void MyClear( ConsoleColor c )
+        {
+            Console.BackgroundColor = c;
+            Console.Clear();
+        }
+
+        private static void DrawLetter( char c, int x, int y,  ConsoleColor fore, ConsoleColor back )
+        {
+            Console.BackgroundColor = back;
+            Console.ForegroundColor = fore;
+            Console.SetCursorPosition( x, y );
+            Console.Write( c );
         }
     }
 }
--- makefile
+++ makefile
@@ -1,4 +1,6 @@
 
+all : 
+	csc Program.cs -out:Game.exe -t:exe
 
 clean : 
 	rm -rf *.exe
