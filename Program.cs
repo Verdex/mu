2faa9ad35a76dd50accc24da8541955318ce50f8
David L. Inman <davidlevere@gmail.com>
2018-11-10 23:22:33 -0500

    inc prog

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 
 using System;
+using System.Collections.Generic;
 
 namespace mu
 {
@@ -7,14 +8,47 @@ namespace mu
     {
         public static void Main()
         {
-            MyClear(ConsoleColor.Black);
             Console.CursorVisible = false;
-            DrawLetter( 'a', 5, 5, ConsoleColor.Blue, ConsoleColor.Black );
-            DrawLetter( 'b', 1, 1, ConsoleColor.Red, ConsoleColor.Black );
-            DrawLetter( 'c', 1, 5, ConsoleColor.Green, ConsoleColor.Black );
-            DrawLetter( 'd', 5, 1, ConsoleColor.Yellow, ConsoleColor.Black );
-            Console.Read();
-            Console.Clear();
+
+            var sx = 0;
+            var sy = 0;
+            var c = '\0';
+            
+            while ( c != 'q' )
+            {
+                var h = Console.WindowHeight;
+                var w = Console.WindowWidth;
+                MyClear(ConsoleColor.Black);
+                DrawLetter( 'a', 5, 5, ConsoleColor.Blue, ConsoleColor.Black );
+                DrawLetter( 'b', 1, 1, ConsoleColor.Red, ConsoleColor.Black );
+                DrawLetter( 'c', 1, 5, ConsoleColor.Green, ConsoleColor.Black );
+                DrawLetter( 'd', 5, 1, ConsoleColor.Yellow, ConsoleColor.Black );
+                var v = Console.ReadKey(true);
+                c = v.KeyChar;
+                if ( 
+
+                //l.Add( T(v.Modifiers) );
+            }
+        }
+
+        private static string T( ConsoleModifiers ms )
+        {
+            // Control and shift shows up but the others dont
+            // multiple control and shift cant be pressed at the same time
+            var s = "";
+            if ( (ms & ConsoleModifiers.Alt) != 0 )
+            {
+                s += " alt ";
+            }
+            if ( (ms & ConsoleModifiers.Control) != 0 )
+            {
+                s += " control ";
+            }
+            if ( (ms & ConsoleModifiers.Shift) != 0 )
+            {
+                s += " shift ";
+            }
+            return s;
         }
 
         private static void MyClear( ConsoleColor c )
