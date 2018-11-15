f4547015488a7fb57948eeb0a624d7e957392e3f
David L. Inman <davidlevere@gmail.com>
2018-11-15 18:35:11 -0500

    test file

---
+++ tests.cs
@@ -0,0 +1,62 @@
+
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace ex.parseA
+{
+    public static class Tests 
+    {
+        public static void Main()
+        {
+
+        }
+
+        private static string _name;
+        private static List<string> _checks = new List<string>();
+        private static void Test( string name, Action a )
+        {
+            _name = name;
+            try
+            {
+                a();
+                DisplayFailedChecks();
+                if ( _checks.Count == 0 )
+                {
+                    Console.WriteLine( $"{_name} : Passed" );
+                }
+            }
+            catch( Exception e )
+            {
+                Console.WriteLine( $"{_name} has thrown exception : {e}" );
+            }
+            finally
+            {
+                _name = "";
+                _checks = new List<string>();
+            }
+        }
+
+        private static void DisplayFailedChecks()
+        {
+            foreach( var check in _checks )
+            {
+                Console.WriteLine( $"{_name} failed check {check}" );
+            }
+        }
+        private static void Check( string m, bool t )
+        {
+            if ( !t ) 
+            {
+                _checks.Add( m );
+            }
+        }
+        private static void Assert( string m, bool t )
+        {
+            if ( !t )
+            {
+                throw new Exception( $"Failed assert {m}" );
+            }
+        }
+    }
+}
